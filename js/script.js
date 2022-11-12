3a29242899f1fc887b74386cb00295d5120616e6
valeriarocioquiroz <vrocioquiroz@gmail.com>
2022-11-12 15:50:11 -0300

    working on filters categories, improves

--- js/script.js
+++ js/script.js
@@ -6,17 +6,25 @@ const $$ = (selector) => document.querySelectorAll(selector);
 // Funciones de clean y show atributos
 
 const cleanAside = () => $("#aside").classList.add("hidden");
-const showAside = () => $("#aside").classList.remove("hidden");
-const cleanFrontPage = () => $("#frontPage").classList.add("hidden");
-const showNewOperationForm = () => {
-    $("#formNewOperation").classList.remove("hidden");
-    inputDate.value = todayDate()
+
+const showAside = () =>{ 
+    $("#aside").classList.remove("hidden");
+    categoriesSelectInput($("#selectCategoryFilters"))
 }
+const cleanFrontPage = () => $("#frontPage").classList.add("hidden");
+
+const showNewOperationForm = () => {
+    $("#formNewOperation").classList.remove("hidden")
+    inputDate.value = todayDate()
+    categoriesSelectInput($("#selectCategory"))
+
+};
+
 const cleanNewOperationForm = () => $("#formNewOperation").classList.add("hidden");
 const showDoneOperations = () => $("#doneOperations").classList.remove("hidden");
 const cleanDoneOperations = () => $("#doneOperations").classList.add("hidden");
-const cleanEditOperationsForm = () => $("#editOperationsForm").classList.remove("hidden");
-const showEditOperationsForm = () => $("#editOperationsForm").classList.add("hidden");
+const cleanEditOperationsForm = () => $("#editOperationsForm").classList.add("hidden");
+const showEditOperationsForm = () => $("#editOperationsForm").classList.remove("hidden");
 const showCategoriesForm = () => $("#categoriesForm").classList.remove("hidden");
 const cleanCategoriesForm = () => $("#categoriesForm").classList.add("hidden");
 const showContainer = () => $("#container").classList.remove("hidden");
@@ -47,7 +55,7 @@ const operationsInfo = () => {
     const type = $("#type").value
     const selectedCategory = $("#selectCategory").value
     var date = $("#date").value
-    const id = getDataInLocalStorage('operations').length + 1
+    const id = makeid()
     return {
         id,
         description,
@@ -58,6 +66,21 @@ const operationsInfo = () => {
     }
 }
 
+/**************************************************/
+
+const makeid = () => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < 10 ; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+
+/*******************************************************/
+
 
 const generateTableOperations = (operations) => {
    
@@ -65,24 +88,25 @@ const generateTableOperations = (operations) => {
 
     if (operations) {
         maybeFiltedOperations = operations;
-      }
-      $("#table").innerHTML = "";
+    }
+      
+    $("#table").innerHTML = "";
 
-     maybeFiltedOperations.map((operation) => {
+    maybeFiltedOperations.map((operation) => {
         const { id, description, selectedCategory, date, amount, type } = operation;
         const className = type === "Gasto" ? "red-500" : "green-500";
         const operator = type === "Gasto" ? "-" : "+";
 
         var formatedDate = correctDate(date)
 
-  $("#table").innerHTML += `
+        $("#table").innerHTML += `
             
             <tr>
                 <td class="pl-0 pr-10 mt-4 pt-0 text-lg font-bold">${description}</td>
                 <td class="mt-0 pt-0 pl-10 text-xs ">${selectedCategory}</td>
                 <td class="mt-0 pt-0 pl-10 text-sm">${formatedDate}</td>
                 <td class="mt-0 pt-0 pl-12 text-lg text-${className} font-bold">${operator}${amount}</td>
-                <td class="pl-8 mt-0 pt-0 text-xs"><button class="mr-4 btnEditOperation" data-id="${id}" onclick="operationEdit(${id})">Editar</button><button class="mr-4 btnDeleteOperation" data-id="${id}" onclick="deleteOperation(${id})">Eliminar</button></td>
+                <td class="pl-8 mt-0 pt-0 text-xs"><button class="mr-4 btnEditOperation" data-id="${id}" onclick="operationEdit('${id}')">Editar</button><button class="mr-4 btnDeleteOperation" data-id="${id}" onclick="deleteOperation('${id}')">Eliminar</button></td>
             </tr>
              
         `;
@@ -135,9 +159,10 @@ const find = (objetsType, id) => {
 
 
 const operationEdit = (id) => {
-  cleanEditOperationsForm();
-  cleanDoneOperations();
-  cleanAside();
+  
+  showEditOperationsForm()
+  cleanDoneOperations()
+  cleanAside()
   const editedOperation = find("operations", id);
   console.log(editedOperation);
   $("#editDescription").value = editedOperation.description;
@@ -146,7 +171,7 @@ const operationEdit = (id) => {
   $("#editSelectedCategory").value = editedOperation.selectedCategory;
   $("#editDate").value = editedOperation.date;
 
-  $("#editContainer").innerHTML = `<button id="edit" data-id="${id}" class=" w-[91px] h-10 text-white border-none ml-2  bg-green-400 rounded-md ml-2" onclick="updateOperation(${id})" >Editar</button>`;
+  $("#editContainer").innerHTML = `<button id="edit" data-id="${id}" class=" w-[91px] h-10 text-white border-none ml-2  bg-green-400 rounded-md ml-2" onclick="updateOperation('${id}')" >Editar</button>`;
 };
 
 
@@ -177,13 +202,15 @@ const updateOperation = (id) => {
   });
 
   saveDataInLocalStorage("operations", updatedOperation);
-  showEditOperationsForm();
+  cleanEditOperationsForm();
   showDoneOperations();
   showAside();
-  //generateTableOperations();
-  test()
+  
+  const optionsFiltered = filterByOptions();
+  const categoriesFiltered = filterByCategories($("#selectCategoryFilters").value, optionsFiltered);
+ 
+  generateTableOperations(categoriesFiltered);
 };
-
 
 
 /*------------------------------------------------------------------*/
@@ -193,6 +220,7 @@ const updateOperation = (id) => {
 
 const removeOperation = (id) => {
   let operations = getDataInLocalStorage("operations");
+  console.log(id)
   operations = operations.filter((operation) => operation.id !== id);
   saveDataInLocalStorage("operations", operations);
   return operations;
@@ -200,10 +228,23 @@ const removeOperation = (id) => {
 
 
 const deleteOperation = (id) => {
+    console.log(id)
     removeOperation(id);
-    test()
+    filterByOptions()
    // return generateTableOperations();
 };
+
+/*******************************************************************/
+
+//CANCELAR operación a editar
+
+$("#cancelEditOperation").addEventListener("click", ()=>{
+  cleanEditOperationsForm()
+  showDoneOperations()
+  showAside()
+})
+
+
 
 
 // evento para hacer desaparecer la portada y aparece el formulario de nueva op
@@ -225,39 +266,66 @@ $("#aCategory").addEventListener("click", () => {
   cleanFrontPage();
   cleanDoneOperations();
   cleanAside();
-  addCategoriesItems();
+  generateCategoriesItems();
   cleanEditCategoryWindow();
 });
 
 
+if (!localStorage.getItem("categories")){
 localStorage.setItem ("categories", JSON.stringify([
-    { id: 1,
+    { id: makeid(),
       nameCategory: "Comida" },
 
-    { id: 2, 
+    { id: makeid(), 
       nameCategory: "Servicios" },
 
-    { id: 3, 
+    { id: makeid(), 
       nameCategory: "Salidas" },
 
-    { id: 4, 
+    { id: makeid(), 
       nameCategory: "Educación" },
 
-    { id: 5,
+    { id: makeid(),
       nameCategory: "Transporte" },
 
-    { id: 6, 
+    { id: makeid(), 
       nameCategory: "Trabajo" },
   ])
 );
+}
 
+//Función que setea las categorías en el input select de nueva operación y en  en el input select de filtros de categorías
+
+
+const categoriesSelectInput = (idInput) =>{
+    let categories = getDataInLocalStorage('categories')
+    idInput.innerHTML = ""
+    for (const category of categories) {
+        const {id, nameCategory} = category
+        idInput.innerHTML += `<option value="${nameCategory}">${nameCategory}</option>`
+
+    }
+}
+
+window.addEventListener('load', ()=>{
+    alert("hola")
+    categoriesSelectInput($("#selectCategoryFilters"))
+    const option = document.createElement("option")
+    const allText = document.createTextNode("Todas")
+    option.appendChild(allText)
+
+    $("#selectCategoryFilters").append(option)
+    
+   // $("#selectCategoryFilters").innerHTML += `<option>Todas</option>`
+})
+    
 
 //AGREGAR CATEGORIAS A UN ARRAY DE OBJETOS EN LOCAL STORAGE
 
 
 const categoriesInfo = () => {
   const nameCategory = $("#nameCategory").value;
-  const id = getDataInLocalStorage("categories").length + 1;
+  const id = makeid();
   return {
     id,
     nameCategory,
@@ -265,8 +333,9 @@ const categoriesInfo = () => {
 };
 
 
-const addCategoriesItems = () => {
+const generateCategoriesItems = () => {
   $("#categoriesItems").innerHTML = "";
+  $("#selectCategory").innerHTML = ""
 
   getDataInLocalStorage("categories").map((item) => {
     const { id, nameCategory } = item;
@@ -278,12 +347,13 @@ const addCategoriesItems = () => {
     
         <div class="flex flex-row w-[200px] mt-8">
             
-            <button class="ml-2" id="${id}" onclick="categoryEdit(${id})">Editar</button>
-            <button class="ml-2">Eliminar</button>
+            <button class="ml-2" id="${id}" onclick="categoryEdit('${id}')">Editar</button>
+            <button class="ml-2" id="${id}" onclick="deleteCategory('${id}')">Eliminar</button>
                 
         </div>
     
-     </div> `;
+    </div> `;
+
   });
 };
 
@@ -294,7 +364,7 @@ $("#addCategory").addEventListener("click", () => {
   categories.push(categoriesInfo());
   saveDataInLocalStorage("categories", categories);
   console.log(categories);
-  addCategoriesItems(getDataInLocalStorage("categories"));
+  generateCategoriesItems(getDataInLocalStorage("categories"));
   $("#nameCategory").value = "";
 });
 
@@ -308,7 +378,7 @@ const categoryEdit = (id) => {
   const editedCategory = find("categories", id);
   $("#editCategoryInput").value = editedCategory.nameCategory;
 
-  $("#editCategoryContainer").innerHTML = `<button id="${id}" class=" w-[91px] h-10 text-white border-none ml-2  bg-green-400 rounded-md ml-2" onclick="updateCategory(${id})">Editar</button>`;
+  $("#editCategoryContainer").innerHTML = `<button id="${id}" class=" w-[91px] h-10 text-white border-none ml-2  bg-green-400 rounded-md ml-2" onclick="updateCategory('${id}')">Editar</button>`;
 };
 
 
@@ -333,9 +403,38 @@ const updateCategory = (id) => {
 
   cleanEditCategoryWindow();
   showCategoriesForm();
-  addCategoriesItems();
+  generateCategoriesItems();
 };
 
+
+/*********************************************************/
+
+// Remover categoría
+
+const removeCategory = (id) =>{
+    let categories = getDataInLocalStorage("categories")
+   
+    categories = categories.filter((category) => category.id !== id);
+    
+    saveDataInLocalStorage("categories", categories)
+    
+}
+
+  
+const deleteCategory = (id) => {
+  removeCategory(id)
+  return generateCategoriesItems()
+}
+
+/*********************************************************/
+
+
+//CANCELAR categoría en edición
+
+$("#cancelCategoryInEdition").addEventListener("click", ()=>{
+    showCategoriesForm()
+    cleanEditCategoryWindow()
+})
 
 /*********************************************************/
 
@@ -345,6 +444,8 @@ $("#navBalance").addEventListener("click", () => {
     showDoneOperations();
     generateTableOperations();
     cleanCategoriesForm();
+    cleanNewOperationForm();
+   // categoriesSelectInput($("#selectCategoryFilters"))
     //falta agregar mas dom
   }
 });
@@ -375,7 +476,7 @@ const correctDate = (date) =>{
 }
 
 
- /*seteando al primer dia del mes**********************************************************/
+/* seteando al primer dia del mes */
 
 window.onload = ()=>{
     const inputFiltersDate = $("#filtersDate")
@@ -458,6 +559,8 @@ const balance = ()=>{
 
 
 /***********************************************************/
+
+
 $("#icon").addEventListener("click",()=>{
     if (!localStorage.getItem('operations')) {
         showFrontPage()
@@ -481,61 +584,82 @@ $("#icon").addEventListener("click",()=>{
 // GASTO, GANANCIA, TODOS
 
 $("#selectType").addEventListener("change", (e) => {
-  switch (e.target.value) {
-    case "Gasto":
-      filterByType("Gasto");
-      break;
 
-    case "Ganancia":
-      filterByType("Ganancia");
-      break;
-
-    default:
-      removeFilters();
-      break;
-  }
+    filterByOptions(e.target.value)
+ 
 });
 
-const filterByType = (type) => {
-  const localS = getDataInLocalStorage("operations");
 
-  const filteredLocalS = localS.filter((operation) => operation.type === type);
+const filterByOptions = () => {
+  const type = $("#selectType").value
 
-  generateTableOperations(filteredLocalS);
-};
+  if(type === "Todos"){
+    generateTableOperations()
+    return getDataInLocalStorage("operations")
+  }else{
 
+    const localS = getDataInLocalStorage("operations");
+  
+    const filteredLocalS = localS.filter((operation) => operation.type === type);
 
-const removeFilters = () => {
-  generateTableOperations();
+    generateTableOperations(filteredLocalS);
+
+    return filteredLocalS
+  }
+
+     
 };
 
 
 // Función que muestra la tabla según el filtro aplicado
 
-test = () => {
-  console.log($("#selectType").selectedOptions[0].id)
-  switch ($("#selectType").selectedOptions[0].id) {
-    case "spentFilter":
-      filterByType("Gasto");
-      break;
 
-    case "earningsFilter":
-      filterByType("Ganancia");
-      break;
-
-    default:
-      removeFilters();
-      break;
-  }
-};
 
 
 /************************************************************************/
 
+//FILTRO POR CATEGORÍAS
+
+$("#selectCategoryFilters").addEventListener("change", (e) => {
+
+    const optionsFiltered = filterByOptions();
+    const categoriesFiltered = filterByCategories(e.target.value, optionsFiltered); 
+
+    generateTableOperations(categoriesFiltered)
+     
+});
+
+
+const filterByCategories = (selectedCategory, operations) => {
+
+    const filteredLocalS = operations.filter((operation) => operation.selectedCategory === selectedCategory);
+
+    return filteredLocalS
+  
+    //generateTableOperations(filteredLocalS);
+    
+};
+
+
+
+
+  
+
+
+
+
+
+
+
+
 if (!localStorage.getItem("operations")) {
     localStorage.setItem("operations", JSON.stringify([]));
-  } else {
+
+   
+} else {
     cleanFrontPage();
     showDoneOperations();
     generateTableOperations();
-  }
+    
+}
+
